9047c6d8915ed5459f3b7bd258c5e6f73dcf7a24
Douglas Rodrigues <123272282+Zatanaris@users.noreply.github.com>
2023-06-01 18:53:49 -0300

    feat: section service html/css finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 const OpenBtn = document.querySelector("#open--menu--btn");
+const CloseBtn = document.querySelector("#close--btn");
 const navbar = document.querySelector(".navbar");
 const header = document.querySelector("header");
 
+const ArrElements = [CloseBtn, OpenBtn];
+
+const eventElements = function (elements, typeEvent, callback) {
+   for (let i = 0, len = elements.length; i < len; i++) {
+      elements[i].addEventListener(typeEvent, callback);
+   }
+};
+
+const toggleNav = function () {
+   navbar.classList.toggle("active");
+};
+
+eventElements(ArrElements, "click", toggleNav);
+
 window.addEventListener("scroll", () => {
    header.classList[window.scrollY > 80 ? "add" : "remove"]("active");
-});
-
-OpenBtn.addEventListener("click", () => {
-   OpenBtn.classList.toggle("bx-x");
-   navbar.classList.toggle("active");
 });
